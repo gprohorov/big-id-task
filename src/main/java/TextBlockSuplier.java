f12410ccc69f85067624efae57194ea820d3e038
gprohorov <george.prokhorov@external.getneteurope.com>
2021-09-24 23:39:35 +0300

    reader.close()

--- src/main/java/TextBlockSuplier.java
+++ src/main/java/TextBlockSuplier.java
@@ -15,10 +15,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -41,11 +38,11 @@ public class TextBlockSuplier {
         List<Map<String, List<Location>>> maps = new ArrayList<>();
         int[] cursor = new int[2];  // [0] - number of a block, [1] - value of the 1st char in a block
         ExecutorService executorService = Executors.newFixedThreadPool(50);
-        new BufferedReader(new InputStreamReader(new URL(urlAsString).openStream()))
-                .lines()
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(urlAsString).openStream()));
+        reader.lines()
                 .forEach(line->{
                     lines.add(line);
-                    if(lines.size() % Constants.LINES_IN_BLOCK == 0 || line == null) {
+                    if(lines.size() % Constants.LINES_IN_BLOCK == 0 ) {
                         String text = getText(lines);
                         TextBlock block = new TextBlock(cursor[0], cursor[1], text);
                         Matcher matcher = new Matcher(block);
@@ -58,8 +55,20 @@ public class TextBlockSuplier {
                         lines.clear();
                     }
                 });
+
+        if (lines.size() > 0){
+            String text = getText(lines);
+            TextBlock block = new TextBlock(cursor[0], cursor[1], text);
+            Matcher matcher = new Matcher(block);
+            CompletableFuture< Void > future =
+                    CompletableFuture.supplyAsync(matcher::getOutputMapForBlock, executorService)
+                            .thenAcceptAsync(maps::add, executorService);
+            futures.add(future);
+            lines.clear();
+        }
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).get();
         executorService.shutdown();
+        reader.close();
         return maps;
     }
 
